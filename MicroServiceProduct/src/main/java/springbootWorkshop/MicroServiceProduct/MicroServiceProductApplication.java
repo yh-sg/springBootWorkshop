c0753f8552cd0a1084f24b4afe3371c4af46d833
yonghan <wangyh.92@gmail.com>
2021-02-12 12:07:49 +0800

    beforeAssignment

--- MicroServiceProduct/src/main/java/springbootWorkshop/MicroServiceProduct/MicroServiceProductApplication.java
+++ MicroServiceProduct/src/main/java/springbootWorkshop/MicroServiceProduct/MicroServiceProductApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableDiscoveryClient
-@EnableEurekaClient
 @SpringBootApplication
 public class MicroServiceProductApplication {
 
